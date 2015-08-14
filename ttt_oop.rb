6e108e34bb2b922d6ba544c9df0fed9cafa343d5
Jerry Khalil <email.jerry@yahoo.com>
2015-08-14 16:13:57 -0500

    functional board marking

--- ttt_oop.rb
+++ ttt_oop.rb
@@ -32,13 +32,18 @@
  require 'pry'
 
 class Square 
-attr_reader :len, :char, :spaces, :top_bottom
+attr_reader :len, :char, :spaces, :top_bottom, :center
 attr_accessor :sq 
 
   def initialize(len,char)
     @len = len
     @char = char
     @sq_model = self.build_square
+  end
+
+  def get_center
+    @center = (len / 2)
+    center
   end
   
   def build_square
@@ -90,24 +95,55 @@ class Board < Square
   def draw_board
 
     loop_counter = 0
+    row = 1
     #num_squares_per_row = 4
     #number_of_rows = 
     #binding.pry
-    break_point = self.len * squares_per_row
+    break_point_max = self.len * squares_per_row
+    break_point = 0
     while loop_counter < self.number_of_rows
     square_index = 0
     while square_index < my_board[0].sq.size
-      my_board.each_index do |index|
-      print my_board[index].sq[square_index]
-      if index == (squares_per_row - 1)
+      while row <= self.number_of_rows
+      if row == 1
+        range_start = 0
+        range_stop = ((squares_per_row * row) - 1)
+        break
+      else
+        range_start = ((row * squares_per_row) - squares_per_row)
+        range_stop = (range_start + (squares_per_row - 1)) 
+        break
+      #elsif row == 2
+      #  range_start = 3
+      #  range_stop = 5
+      #  break
+      #else
+      #  range_start = 6
+      #  range_stop = 8
+      #  break
+      end
+    end
+      my_board.each_slice(squares_per_row)  do |index|
+      #my_board[range_start..range_stop].each_index do |index|
+      print my_board[range_start].sq[square_index]
+      #range_start += 1
+      if range_start == range_stop
         puts "\n"
         square_index += 1
         break
+        else
+        range_start += 1
+
+      #if index == (squares_per_row - 1)
+      #  puts "\n"
+      #  square_index += 1
+      #  break
       end
       end
     end
-binding.pry
+#binding.pry
 loop_counter += 1
+row += 1
   end
     
   end
@@ -119,7 +155,7 @@ class Tic_Tac_Toe_Board < Board
 
   def initialize
     #@ttt_square = Square.new(6,"*")
-    @ttt_board = Board.new(3,3, 6, "*")  
+    @ttt_board = Board.new(3,3, 6, ".")  
     ttt_board.build_board
     #binding.pry
   end
@@ -128,8 +164,8 @@ class Tic_Tac_Toe_Board < Board
     ttt_board.draw_board
   end
 
-  def ttt_mark_square
-    ttt_board.my_board[0].sq[2] = "   X  "
+  def ttt_mark_square(square_to_mark)
+    ttt_board.my_board[square_to_mark].sq[(ttt_board.len / 2)] = ".  X ."
   end
 
 end
@@ -165,7 +201,7 @@ class Game
     game_board.ttt_draw_board
     @player_one = Player.new("Bob","X")
     @player_two = Player.new("Opponent", "O")
-    game_board.ttt_mark_square
+    game_board.ttt_mark_square(2)
     game_board.ttt_draw_board
     #binding.pry
   end
