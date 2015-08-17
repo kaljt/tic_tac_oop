82468f45bf0cc1853d33ddfe8d95b667b9f4cdf1
Jerry Khalil <email.jerry@yahoo.com>
2015-08-17 07:09:19 -0500

    changing square class

--- ttt_redux.rb
+++ ttt_redux.rb
@@ -2,20 +2,34 @@
  require 'pry'
 
 class Square 
-attr_reader :len, :char, :spaces, :top_bottom, :center
-attr_accessor :sq, :occupied
+  attr_reader :len, :char, :spaces, :top_bottom, :center
+  attr_accessor :sq, :occupied, :value
 
-  def initialize(len,char)
+  def initialize(len,char,value)
+    @value = value
     @len = len
     @char = char
-    @sq_model = self.build_square
+    @sq_model = build_square
   end
 
   def get_center
     @center = (len / 2)
     center
   end
+
+  def mark(marker)
+    @value = marker
+  end
   
+  def is_occupied?
+    @value != " "
+    #sq[get_center]
+  end
+
+  def is_empty?
+    @value = " "
+  end
+
   def build_square
     @sq = Array.new(len)
     @spaces = (" " * (len - 2))
@@ -33,9 +47,6 @@ attr_accessor :sq, :occupied
       break if counter == len
     #binding.pry
    end
-   def display_square
-     sq 
-   end
  end
 end
 
@@ -44,25 +55,45 @@ class Board < Square
   attr_accessor :my_board
 
   def initialize(sq_per_row, num_of_rows, length, chr)
+    @data = {}
     @squares_per_row = sq_per_row
     @number_of_rows = num_of_rows
     @len = length
     @char = chr
+    (1..(squares_per_row * number_of_rows)).each {|position| @data[position] = Square.new(@length,@char," ")}
     #@square_model= sq_model
   end
-
-  def build_board
-    #binding.pry
-    @my_board = Array.new(squares_per_row * number_of_rows)
-    my_board.each_index do |index|
-    my_board[index] = Square.new(self.len,self.char)
-    end
-    my_board.each do |square|
-    square.build_square
-    end
-    #binding.pry
+  def draw
+    puts "Drawing....board..."
+    puts @data.inspect
   end
 
+  def all_squares_marked?
+    empty_squares.size == 0
+  end
+
+  def empty_squares
+    #returns array
+    @data.select {|_, square| square.value == " "}.values
+  end
+
+  def empty_positions
+    @data.select {|_, square| square.is_empty? }.keys
+  end
+
+
+  #def build_board
+  #  #binding.pry
+  #  @my_board = Array.new(squares_per_row * number_of_rows)
+  #  my_board.each_index do |index|
+  #  my_board[index] = Square.new(self.len,self.char)
+  #  end
+  #  my_board.each do |square|
+  #  square.build_square
+  #  end
+  #  #binding.pry
+  #end
+  
   def draw_board
 
     loop_counter = 0
@@ -127,11 +158,11 @@ class Tic_Tac_Toe_Board < Board
   def initialize
     #@ttt_square = Square.new(6,"*")
     @ttt_board = Board.new(3,3, 6, ".")  
-    ttt_board.build_board
+    #ttt_board.build_board
   end
 
   def ttt_draw_board
-    ttt_board.draw_board
+    ttt_board.draw
   end
 
   def ttt_mark_square(square_to_mark,player)
@@ -146,6 +177,10 @@ class Tic_Tac_Toe_Board < Board
     ttt_board.my_board[square_to_mark].sq[(ttt_board.len / 2)] = ".  X ."
     ttt_board.my_board[square_to_mark].occupied = 1
   end
+  end
+
+  def all_squares_marked?
+    
   end
 
 end
@@ -185,49 +220,70 @@ class Game
 
   include Turn
   attr_accessor :player_one, :player_two, :game_board
+  attr_reader :marker
 
-  def start_game
-    
-    @game_board = Tic_Tac_Toe_Board.new
-    game_board.ttt_draw_board
-    @player_one = Player.new("Bob","X")
-    @player_two = Player.new("Opponent", "O")
-    binding.pry
-    #game_board.ttt_mark_square(2)
-    #game_board.ttt_draw_board
-    while !game_over
-    player_choice = player_one.choose_square
-    game_board.ttt_mark_square(player_choice,player_one)
-    game_board.ttt_draw_board
-    player_choice = player_two.opponent_choose_square
-    game_board.ttt_mark_square(player_choice,player_two)
-    game_board.ttt_draw_board
+  def initialize
+    @board = Tic_Tac_Toe_Board.new
+    @player_one = Player.new("JoJo","X")
+    @player_two = Player.new("Cylon","O")
+    @current_player = @player_one
   end
+
+  def current_player_marks_square
+    if @current_player == @player_one
+      begin
+        puts "Choose a position (1-9):"
+        position = gets.chomp.to_i
+      end until @board.empty_positions.include?(position)
+      else
+        position = @board.empty_positions.sample
+      end
+      @board[position].mark(@current_player.player_marker)
+  end
+
+  def alternate_player
+    if @current_player == @player_one
+      @current_player = @player_two
+    else
+      @current_player = @player_one
+    end
+  end
+
+  def play
+    @board.draw
+    loop do 
+      current_player_marks_square
+      alternate_player
+    end
+  end
+end
+    #@game_board = Tic_Tac_Toe_Board.new
+    #game_board.ttt_draw_board
+    #@player_one = Player.new("Bob","X")
+    #@player_two = Player.new("Opponent", "O")
+    #binding.pry
+    ##game_board.ttt_mark_square(2)
+    ##game_board.ttt_draw_board
+    #while !game_over
+    #player_choice = player_one.choose_square
+    #game_board.ttt_mark_square(player_choice,player_one)
+    #game_board.ttt_draw_board
+    #player_choice = player_two.opponent_choose_square
+    #game_board.ttt_mark_square(player_choice,player_two)
+    #game_board.ttt_draw_board
+  
 
     #binding.pry
-  end
 
-  def winner
-    
-  end
-
-  def game_over
-    
-  end
-
-end
-
-class Player < Game
+class Player
   include Turn
   attr_reader :player_name, :player_marker
 
   def initialize(p_name,p_mark = nil)
     @player_name = p_name
     @player_marker = p_mark
-    
   end
 end
 
 
-my_game = Game.new
-my_game.start_game
+Game.new.play
